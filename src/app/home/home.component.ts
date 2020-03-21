f10806cb47ebb48cb10e5872de861798d3605206
Julius Schinschke <juliuss.16303@gmail.com>
2020-03-22 00:31:39 +0100

    Create Course Interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Course} from '../models/classroom';
+import {User} from '../models/user';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +11,22 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  user: User = new class implements User {
+    email: string;
+    id: number;
+    nickname: string;
+    password: string;
+    username: string;
+  }
+
+  course: Course[];
+
+  getCourse(): void {
+  }
+
   ngOnInit(): void {
   }
+
+
 
 }
