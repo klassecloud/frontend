0ba69a9c13f6543bd19fc3e3e86636b8028414b5
Julius Schinschke <juliuss.16303@gmail.com>
2020-03-21 23:00:05 +0100

    Complete Registration Form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -13,35 +13,46 @@ export class RegisterComponent implements OnInit {
   user: User = new class implements User {
     email: string;
     id: number;
-    nickname: string;
-    password: string;
-    username: string;
+    nickname: null;
+    password: null;
+    username: null;
   };
-  passwordMatch: boolean;
-  passwordToShort: boolean = true;
+  repeatPassword: null;
+  passwordDontMatch: boolean;
+  passwordToShort: boolean;
+  nicknameEmpty: boolean;
+  usernameEmpty: boolean;
 
   ngOnInit(): void {
   }
-
-  isPasswordLongEnough(): boolean {
-    return true;
-  }
-
-  isPasswordMatching(passwordRepeatEvent: any): void {
-    const passwordRepeat = passwordRepeatEvent.target.value;
-    if (!this.passwordToShort)
-      this.passwordMatch = passwordRepeat === this.user.password;
+  isPasswordLongEnough(): void {
+    if (this.user.password == null || this.user.password.length < 8)
+      this.passwordToShort = true;
     else
-
+      this.passwordToShort = false;
+  }
+  isPasswordMatching(): void {
+    if (this.repeatPassword == null)
+      this.passwordDontMatch = true;
+    else
+      this.passwordDontMatch = !(this.repeatPassword === this.user.password);
+  }
+  isUsernameEmpty(): void {
+    this.usernameEmpty = this.user.username == null || this.user.username.length < 1;
+  }
+  isNicknameEmpty(): void {
+    this.nicknameEmpty = this.user.nickname == null || this.user.nickname.length < 1;
+  }
+  signUp(): void {
+      this.isPasswordLongEnough();
+      this.isPasswordMatching();
+      this.isUsernameEmpty();
+      this.isNicknameEmpty();
+      if (!this.passwordToShort && !this.passwordDontMatch && !this.nicknameEmpty && !this.usernameEmpty) {
+        alert('Hello, World!');
+      }
   }
 
-  validateNoEmptyInputFields(): boolean {
-    return;
-  }
-
-  validateUserInput(): boolean {
-    return this.validateNoEmptyInputFields() && this.passwordMatch;
-  }
 }
 
 
